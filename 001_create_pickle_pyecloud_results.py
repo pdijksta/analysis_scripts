3308f6beb8b3dfe113ff3a6ba4432116a0a8ec25
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2016-10-11 11:58:00 +0200

    changed structure of output dict

--- 001_create_pickle_pyecloud_results.py
+++ 001_create_pickle_pyecloud_results.py
@@ -9,24 +9,45 @@ import scipy.io as sio
 import numpy as np
 from scipy.constants import e as const_e
 
-const_LHC_frev = 11.2455e3
+# Config
+
 root_dir = './all_data'
 
 hl_dict = {}
 all_files = os.listdir(root_dir)
-file_re = re.compile('^Fill(\d+)_cut(\d+\.\d[1-9]*)0*h_\d+GeV_for_triplets_(B[1,2])_LHC_([A-Za-z]+)_\d+GeV_sey([\d\.]+)_coast([\d\.]+)$')
+# Regular Expression for the folder names
+folder_re = re.compile('^Fill(\d+)_cut(\d+\.\d[1-9]*)0*h_\d+GeV_for_triplets_(B[1,2])_LHC_([A-Za-z]+)_\d+GeV_sey([\d\.]+)_coast([\d\.]+)$')
 
 fail_ctr = 0
 success_ctr = 0
 
 fail_lines = ''
 fail_lines_IO = ''
+const_LHC_frev = 11.2455e3
 
-for f in all_files:
-    file_info = re.search(file_re,f)
+def insert_to_nested_dict(dictionary, value, keys, must_enter=False, add_up=False):
+    """ 
+    Inserts value to nested dictionary. The location is specified by keys.
+    If must_enter is set to True, an error is raised if the entry is already present.
+    """
+    for key in keys[:-1]:
+        if key not in dictionary:
+            dictionary[key] = {}
+        dictionary = dictionary[key]
+
+    last_key = keys[-1]
+    if last_key not in dictionary:
+        dictionary[last_key] = value
+    elif add_up:
+        dictionary[last_key] += value
+    elif must_enter:
+        raise ValueError('Key %s already exists!' % last_key)
+
+
+for folder in all_files:
+    file_info = re.search(folder_re,folder)
     if file_info is None:
         continue
-
 
     filln = file_info.group(1)
     time_of_interest = file_info.group(2)
@@ -37,11 +58,11 @@ for f in all_files:
     coast = file_info.group(6)
     print(filln,time_of_interest,beam,device,sey,coast)
 
-    mat_str = root_dir + '/' + f + '/Pyecltest.mat'
+    mat_str = root_dir + '/' + folder + '/Pyecltest.mat'
     if not os.path.isfile(mat_str):
         print('Warning: file %s does not exist' % mat_str)
         fail_ctr += 1
-        fail_lines += f + '\n'
+        fail_lines += folder + '\n'
         continue
 
 
@@ -51,30 +72,24 @@ for f in all_files:
     except IOError:
         print('IOError')
         fail_ctr += 1
-        fail_lines_IO += f + '\n'
+        fail_lines_IO += folder + '\n'
         continue
     else:
         success_ctr += 1
 
     heatload = np.sum(matfile['energ_eV_impact_hist'])*const_LHC_frev*const_e
+#    e_transverse_hist = np.sum(matfile['nel_hist'],axis=0)
+#    print(e_transverse_hist)
+#    sys.exit()
 
-    if main_key not in hl_dict.keys():
-        hl_dict[main_key] = {}
-    if device not in hl_dict[main_key].keys():
-        hl_dict[main_key][device] = {}
-    if coast not in hl_dict[main_key][device].keys():
-        hl_dict[main_key][device][coast] = {}
-    if sey not in hl_dict[main_key][device][coast].keys():
-        hl_dict[main_key][device][coast][sey] = [0, 0]
+    insert_to_nested_dict(hl_dict, heatload, [main_key,device,coast,sey,'Total'], add_up=True)
+    insert_to_nested_dict(hl_dict, 1, [main_key,device,coast,sey,'Beam_nr'], add_up=True)
+    insert_to_nested_dict(hl_dict, heatload, [main_key, device, coast, sey, beam], must_enter=True)
 
-    # add up Beams 1 and 2
-    hl_dict[main_key][device][coast][sey][0] += heatload
-    hl_dict[main_key][device][coast][sey][1] += 1
+print(hl_dict)
 
-#print(hl_dict)
-
-with open('./heatload_pyecloud.pkl','w') as pkl:
-    cPickle.dump(hl_dict,pkl)
+#with open('./heatload_pyecloud.pkl','w') as pkl:
+#    cPickle.dump(hl_dict,pkl)
 
 print(fail_ctr, success_ctr)
 print(fail_lines)
