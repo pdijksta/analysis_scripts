ec52620670c4c67bb261e897da62ed2a9b947c39
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2016-10-14 12:23:58 +0200

    greatly improved script
    * proton / electron number now in separate figure
    * Good display of Drift / Quad / Dipole contribution for different
    * coasting beams

--- 002_bunch_by_bunch_coasting.py
+++ 002_bunch_by_bunch_coasting.py
@@ -1,8 +1,10 @@
 import sys
+import argparse
+
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
-import argparse
+from scipy.constants import e as qe
 
 import LHCMeasurementTools.myfilemanager as mlo
 import LHCMeasurementTools.mystyle as ms
@@ -24,7 +26,6 @@ assumed_quad_sey = args.q
 assumed_drift_sey = args.d
 
 T_rev = 88.9e-6
-qe = 1.60217657e-19
 
 main_folder = './all_data'
 beam_snapshot_folder = './beam_snapshots'
@@ -84,34 +85,41 @@ quad_contribution_per_hc = quad_contribution_per_meter * l_quad
 drift_contribution_per_hc = drift_contribution_per_meter * l_drift
 
 plt.close('all')
-fig = plt.figure()
-title_str = 'Comparison of measured to simulated bunch by bunch power losses %s.' % args.beam
-fig.canvas.set_window_title(title_str)
-title_str += '\nAssumed Quad/Drift SEY: %.2f/%.2f' % (assumed_quad_sey,assumed_drift_sey)
-plt.suptitle(title_str,fontsize=25)
 
-sp0 = plt.subplot(3,1,1)
+###
+fig = plt.figure()
+title_str = 'Electrons and protons in the machine'
+fig.canvas.set_window_title(title_str)
+plt.suptitle(title_str, fontsize=25)
+
+sp0 = plt.subplot(2,1,1)
 sp0.plot(ob_snapshot.ppb_vect, color=colorbeam,label='Measured')
 sp0.set_ylabel('Bunch intensity [p$^+$]')
 sp0.set_xlim(0,3500)
 
-sp1 = plt.subplot(3,1,2, sharex=sp0)
-sp1.set_ylabel('Electrons [1/m]')
+sp_el = plt.subplot(2,1,2, sharex=sp0)
+sp_el.set_ylabel('Electrons [1/m]')
 
-sp2 = plt.subplot(3,1,3, sharex=sp1)
-sp2.set_ylabel('Power loss [W/hc]')
-sp2.set_xlabel('25 ns slot')
+###
+fig = plt.figure()
+title_str = 'Comparison of measured to simulated bunch by bunch power losses %s.' % args.beam
+fig.canvas.set_window_title(title_str)
+title_str += '\nAssumed Quad/Drift SEY: %.2f/%.2f' % (assumed_quad_sey,assumed_drift_sey)
+plt.suptitle(title_str, fontsize=25)
 
 t_bun_zeros = np.zeros_like(t_bun[:-1])
 t_bun_ones = np.ones_like(t_bun_zeros)
-sp2.fill_between(t_bun[:-1]/25e-9, t_bun_zeros, drift_contribution_per_hc, alpha=0.4, color='red', label='Drift')
-sp2.fill_between(t_bun[:-1]/25e-9, drift_contribution_per_hc, drift_contribution_per_hc+quad_contribution_per_hc, alpha=0.4, color='blue', label='Quad')
-sp2.plot(meas_loss_per_hc, '.-', color='k', label='Measured', marker=None)
 
-# plot simulation data
+sp = None
 for coast_ctr, coast_str in enumerate(coast_strs):
+    sp = plt.subplot(3,1,coast_ctr+1, sharex=sp)
+    sp.set_ylabel('Power loss [W/hc]')
+    sp.fill_between(t_bun[:-1]/25e-9, t_bun_zeros, drift_contribution_per_hc, alpha=0.4, color='red', label='Drift 0.5 $\cdot10^9$ coasting')
+    sp.fill_between(t_bun[:-1]/25e-9, drift_contribution_per_hc, drift_contribution_per_hc+quad_contribution_per_hc, alpha=0.4, color='blue', label='Quad 0.5 $\cdot10^9$ coasting')
+    sp.plot(meas_loss_per_hc, '.-', color='k', label='Measured', marker=None)
+    sp.set_title('Dipole coasting beam %s$\cdot10^9$' % coast_str)
+
     for sey_ctr, sey in enumerate(sey_vect):
-        #print beam_snapshot, device_name, sey
 
         try:
             dip_contribution_per_meter, ob, t_bun = get_simulation_ob('ArcDipReal', sey, coast_str)
@@ -124,7 +132,7 @@ for coast_ctr, coast_str in enumerate(coast_strs):
                 label = None
             color_curr = ms.colorprog(sey_ctr, N_sims)
 
-            sp1.semilogy(ob.t/25e-9, ob.Nel_timep, color=color_curr, label=label, ls=coast_linestyle_dict[coast_str])
+            sp_el.semilogy(ob.t/25e-9, ob.Nel_timep, color=color_curr, label=label, ls=coast_linestyle_dict[coast_str])
 
             if coast_ctr == 0:
                 label = sey
@@ -132,15 +140,20 @@ for coast_ctr, coast_str in enumerate(coast_strs):
                 label = None
 
             ms.sciy()
-            sp2.plot(t_bun[:-1]/25e-9, sim_loss_per_hc, '.-', color=color_curr, label=label, ls=coast_linestyle_dict[coast_str], marker=None)
+            sp.plot(t_bun[:-1]/25e-9, sim_loss_per_hc, '.-', color=color_curr, label=label, marker=None)
 
         except IOError as err:
             print('Got:', err)
+            raise
 
-sp1.legend(bbox_to_anchor=(1, 1.02),  loc='upper left')
-sp2.legend(bbox_to_anchor=(1, 1.02),  loc='upper left', title='Dip SEY')
+    if coast_ctr == 0:
+        sp.legend(bbox_to_anchor=(1, 1.02),  loc='upper left', title='Dip SEY')
+    elif coast_ctr == 2:
+        sp.set_xlabel('25 ns slot')
 
-# Figures for devices only
+sp_el.legend(bbox_to_anchor=(1, 1.02),  loc='upper left')
+
+###
 fig_all_nr = len(coast_strs)
 for fig_nr in xrange(len(coast_strs)+1):
     min_bunch_nr = 3000
