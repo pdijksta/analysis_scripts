7ca6b0e60bc2b21a228b2e2f0606c8f401b502b9
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2016-10-14 19:22:40 +0200

    started doing hl vs intensity plots

--- d000_analysis/devices.py
+++ d000_analysis/devices.py
@@ -4,14 +4,21 @@ import itertools
 
 from pyecloud_device import pyecloud_device
 
-def main(device_list,device_labels_dict, sey_list, coast_strs, dict_keys, hl_pyecloud,
-        scenarios_labels_dict, length, coast_linestyle_dict):
+intensity_list = ['0.7e11', '0.9e11', '1.1e11']
+energy_list = ['450GeV', '6.5TeV']
+
+intensity_list_float = [float(string) for string in intensity_list]
+
+
+def main(devices,device_labels_dict, sey_list, coast_strs, dict_keys, hl_pyecloud,
+        scenarios_labels_dict, length, coast_linestyle_dict, get_energy, get_intensity):
 
     one_list = np.ones_like(sey_list)
-    pyecloud_device_easy = lambda device, coast_str: pyecloud_device(device, coast_str, device_list, coast_strs, hl_pyecloud)
+    pyecloud_device_easy = lambda device, coast_str: pyecloud_device(device, coast_str, devices, coast_strs, hl_pyecloud)
 
     title_str = 'Heat loads for different devices and scenarios, per m and scaled'
 
+    ##
     fig_all_nr = 2
     for fig_nr in (1,2):
         fig = plt.figure()
@@ -19,7 +26,7 @@ def main(device_list,device_labels_dict, sey_list, coast_strs, dict_keys, hl_pye
         plt.suptitle(title_str,fontsize=25)
 
 
-        for dev_ctr, device in enumerate(device_list):
+        for dev_ctr, device in enumerate(devices):
             sp = plt.subplot(2,2,dev_ctr+1)
             title = device_labels_dict[device]
             sp.set_title(title,fontsize=20)
@@ -47,8 +54,21 @@ def main(device_list,device_labels_dict, sey_list, coast_strs, dict_keys, hl_pye
                         sp.plot(sey_list,data[sce_ctr,:],label=label,color=color, ls=ls)
 
             if dev_ctr == 1 or dev_ctr == 2 and fig_nr == fig_all_nr:
-                sp.legend(bbox_to_anchor=(1.1, 1),loc='upper left',fontsize=18)
+                sp.legend(bbox_to_anchor=(1.1, 1),loc='upper left')
 
             axes_factor = length[device]
             unscaled_min, unscaled_max =  sp.get_ylim()
             sp2.set_ylim(axes_factor*unscaled_min,axes_factor*unscaled_max)
+
+    ##
+    data = np.zeros(shape=(len(devices),len(energy_list), len(intensity_list_float), len(coast_strs), len(sey_list)))
+    for key_ctr, key in enumerate(dict_keys):
+        energy = get_energy(key)
+        energy_ctr = energy_list.index(energy)
+        intensity = get_intensity(key)
+        intensity_ctr = intensity_list.index(intensity)
+        for device_ctr, device in enumerate(devices):
+            for coast_ctr, coast_str in enumerate(coast_strs):
+                data[device_ctr,energy_ctr,intensity_ctr,coast_ctr,:] = hl_pyecloud[key_ctr,dev_ctr,coast_ctr,:]
+
+#    for 
