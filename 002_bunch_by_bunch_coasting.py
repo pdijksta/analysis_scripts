39c7dda9cfe9eef4415f7a0c32862cbd20e17994
Philipp Dijkstal <philipp.dijkstal@cern.ch>
2016-10-14 11:20:42 +0200

    added fill_between

--- 002_bunch_by_bunch_coasting.py
+++ 002_bunch_by_bunch_coasting.py
@@ -80,14 +80,33 @@ def get_simulation_ob(device_name, sey, coast_str='0.5'):
 quad_contribution_per_meter, quad_ob,t_bun = get_simulation_ob('ArcQuadReal', assumed_quad_sey)
 drift_contribution_per_meter, drift_ob,t_bun = get_simulation_ob('Drift', assumed_drift_sey)
 
+quad_contribution_per_hc = quad_contribution_per_meter * l_quad
+drift_contribution_per_hc = drift_contribution_per_meter * l_drift
+
 plt.close('all')
 fig = plt.figure()
-title_str = 'Comparison of measured to simulated bunch by bunch power losses.'
+title_str = 'Comparison of measured to simulated bunch by bunch power losses %s.' % args.beam
 fig.canvas.set_window_title(title_str)
 title_str += '\nAssumed Quad/Drift SEY: %.2f/%.2f' % (assumed_quad_sey,assumed_drift_sey)
 plt.suptitle(title_str,fontsize=25)
+
 sp0 = plt.subplot(3,1,1)
+sp0.plot(ob_snapshot.ppb_vect, color=colorbeam,label='Measured')
+sp0.set_ylabel('Bunch intensity [p$^+$]')
+sp0.set_xlim(0,3500)
+
 sp1 = plt.subplot(3,1,2, sharex=sp0)
+sp1.set_ylabel('Electrons [1/m]')
+
+sp2 = plt.subplot(3,1,3, sharex=sp1)
+sp2.set_ylabel('Power loss [W/hc]')
+sp2.set_xlabel('25 ns slot')
+
+t_bun_zeros = np.zeros_like(t_bun[:-1])
+t_bun_ones = np.ones_like(t_bun_zeros)
+sp2.fill_between(t_bun[:-1]/25e-9, t_bun_zeros, drift_contribution_per_hc, alpha=0.4, color='red', label='Drift')
+sp2.fill_between(t_bun[:-1]/25e-9, drift_contribution_per_hc, drift_contribution_per_hc+quad_contribution_per_hc, alpha=0.4, color='blue', label='Quad')
+sp2.plot(meas_loss_per_hc, '.-', color='k', label='Measured', marker=None)
 
 # plot simulation data
 for coast_ctr, coast_str in enumerate(coast_strs):
@@ -96,7 +115,8 @@ for coast_ctr, coast_str in enumerate(coast_strs):
 
         try:
             dip_contribution_per_meter, ob, t_bun = get_simulation_ob('ArcDipReal', sey, coast_str)
-            sim_loss_per_hc = dip_contribution_per_meter * l_dip + quad_contribution_per_meter * l_quad + drift_contribution_per_meter * l_drift
+            dip_contribution_per_hc = dip_contribution_per_meter * l_dip
+            sim_loss_per_hc = dip_contribution_per_hc + quad_contribution_per_hc + drift_contribution_per_hc
 
             if sey_ctr == 0:
                 label = coast_str + ' e9 coasting'
@@ -111,35 +131,22 @@ for coast_ctr, coast_str in enumerate(coast_strs):
             else:
                 label = None
 
-            sp2 = plt.subplot(3,1,3, sharex=sp1)
             ms.sciy()
-            sp2.plot(t_bun[:-1]/25e-9, sim_loss_per_hc, '.-', color=color_curr, label=label, ls=coast_linestyle_dict[coast_str])
+            sp2.plot(t_bun[:-1]/25e-9, sim_loss_per_hc, '.-', color=color_curr, label=label, ls=coast_linestyle_dict[coast_str], marker=None)
 
         except IOError as err:
             print('Got:', err)
 
-sp0.plot(ob_snapshot.ppb_vect, color=colorbeam,label='Measured')
-sp0.set_ylabel('Bunch intensity [p$^+$]')
-sp0.set_xlim(0,3500)
-
-sp1.set_ylabel('Electrons [1/m]')
-
-sp2.plot(meas_loss_per_hc, '.-', color='k', label='Measured')
-sp2.set_ylabel('Power loss [W/hc]')
-sp2.set_xlabel('25 ns slot')
-
 sp1.legend(bbox_to_anchor=(1, 1.02),  loc='upper left')
-sp2.legend(bbox_to_anchor=(1, 1.02),  loc='upper left')
+sp2.legend(bbox_to_anchor=(1, 1.02),  loc='upper left', title='Dip SEY')
 
-
-# Figure for devices only
-
+# Figures for devices only
 fig_all_nr = len(coast_strs)
 for fig_nr in xrange(len(coast_strs)+1):
     min_bunch_nr = 3000
     fig = plt.figure()
     window_title_str = 'Simulated bunch by bunch power losses for different devices %i.' % fig_nr
-    fig.canvas.set_window_title(title_str)
+    fig.canvas.set_window_title(window_title_str)
     if fig_nr == fig_all_nr:
         suptitle_str = 'Simulated bunch power losses'
     else:
